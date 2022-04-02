8d69bad578d072b9b9ea0fc746250e74e6933055
ldw159141 <1244943676@qq.com~>
2022-04-03 01:00:52 +0800

    Site updated: 2022-04-03 01:00:51

--- js/text.js
+++ js/text.js
@@ -1,4 +1,4 @@
-//动态彩带bynote.cn
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>//动态彩带bynote.cn
 (function (name, factory) {
     if (typeof window === "object") {
         window[name] = factory()
--- js/text2.js
+++ js/text2.js
@@ -1,4 +1,4 @@
-"use strict";
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>"use strict";
 
 function updateCoords(e) {
     pointerX = (e.clientX || e.touches[0].clientX) - canvasEl.getBoundingClientRect().left, pointerY = e.clientY || e.touches[
